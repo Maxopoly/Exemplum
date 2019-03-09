b8f4b7bb69ca1617fa5fcf83a7c17ad9f12f4d32
Maxopoly <maxopoly3@gmail.com>
2019-03-09 17:50:31 +0100

    Use correct quotations

--- setup.sh
+++ setup.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-author=TODO
-pluginName=TODO
-groupId=TODO
+author=Maxopoly
+pluginName=SuperNewPlugin
+groupId=com.github.kingdada
 mainPluginClass="$groupId.$pluginNamePlugin"
 
 function replaceOccurences() {
-	find src -type f -print0 | xargs -0 sed -i 's/$0/$1/g'
-	sed -i 's/$0/$1/g' pom.xml
-        sed -i 's/$0/$1/g' license.txt
+	echo sed -i "s/$1/$2/g" pom.xml
+	find src -type f -print0 | xargs -0 sed -i "s/$1/$2/g"
+	sed -i "s/$1/$2/g" pom.xml
+        sed -i "s/$1/$2/g" license.txt
 }
 
 
-replaceOccurences() "GROUP_ID_REPLACE_ME" "$groupId"
-replaceOccurences() "AUTHOR_REPLACE_ME" "$author"
-replaceOccurences() "PLUGIN_NAME_REPLACE_ME" "$pluginName"
-replaceOccurences() "MAIN_PLUGIN_CLASS" "$mainPluginClass"
+replaceOccurences "GROUP_ID_REPLACE_ME" "$groupId"
+replaceOccurences "AUTHOR_REPLACE_ME" "$author"
+replaceOccurences "PLUGIN_NAME_REPLACE_ME" "$pluginName"
+replaceOccurences "MAIN_PLUGIN_CLASS" "$mainPluginClass"
